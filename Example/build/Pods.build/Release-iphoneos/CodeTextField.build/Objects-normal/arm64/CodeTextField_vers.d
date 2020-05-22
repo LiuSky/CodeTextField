f79398e405f7f8a29ac22af9404dc07ee8ffa8f8
Liuxiaobin <327847390@qq.com>
2020-05-22 15:22:26 +0800

    -deleted: build log

--- Example/build/Pods.build/Release-iphoneos/CodeTextField.build/Objects-normal/arm64/CodeTextField_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/xiaobinliu/Desktop/Github/CodeTextField/Example/build/Pods.build/Release-iphoneos/CodeTextField.build/DerivedSources/CodeTextField_vers.c
--- Example/build/Pods.build/Release-iphoneos/CodeTextField.build/Objects-normal/armv7/CodeTextField-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/xiaobinliu/Desktop/Github/CodeTextField/Example/Pods/Target\ Support\ Files/CodeTextField/CodeTextField-dummy.m \
-  /Users/xiaobinliu/Desktop/Github/CodeTextField/Example/build/Pods.build/Release-iphoneos/CodeTextField.build/module.modulemap \
-  /Users/xiaobinliu/Desktop/Github/CodeTextField/Example/Pods/Target\ Support\ Files/CodeTextField/CodeTextField-prefix.pch
